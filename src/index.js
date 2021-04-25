8a7bd2222fafefbd1e04628855e1e0914342d0ee
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2021-04-25 21:57:09 +0100

    Day 1 Complete

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const toHtml = (generatedHtml, options = { render: false }) => {
   window.collectHtml = window.collectHtml || [];
 
   // * Add to the main HTML collection
-  window.collectHtml.push(generatedHtml);
+  const generate = generatedHtml ? window.collectHtml.push(generatedHtml) : "";
 
   // * Return HTML to page
   const returnHtml = (x) => {
-    document.getElementById("app").innerHTML = x;
+    document.getElementById("app").innerHTML = x.join("");
     window.collectHtml = "";
     return true;
   };
@@ -27,47 +27,80 @@ const times = (x) => (f) => {
   }
 };
 
+toHtml(`<h1>Vanilla Accordian - Day #1</h1>`);
+
 // * Render our HTML with 3 times with the toHtml function.
 times(3)(() =>
   toHtml(`
-<h1>Vanilla Accordian</h1>
+<h2>Vanilla Accordian</h2>
 <section>
   <ul class="accordian">
     <li>
-      <span>Accordian 1
+      <span>Accordian 1  2 3 123
       3 4</span>
       <p>This is the accordian space!</p>
     </li>
     <li>
       <span>Accordian 2</span>
-      <p>This is the accordian space!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae auctor justo. Donec ac urna sit amet nisl tempor dictum. Ut maximus est id ligula interdum, et posuere augue facilisis. Praesent ipsum neque, rutrum id felis ut, scelerisque gravida ante. Donec justo mi, consequat eget nibh at, ornare scelerisque justo. In ullamcorper feugiat justo, in fringilla justo tristique eget. Donec a diam accumsan, mattis tellus ac, faucibus quam. Donec a magna lacinia, facilisis leo non, commodo nulla. Sed malesuada vel lectus non elementum. Nam vel fermentum enim. Duis lobortis velit non dignissim ultrices. Pellentesque sagittis quam eget varius placerat. Quisque nec eleifend orci, a ullamcorper justo.</p>
     </li>
     <li>
       <span>Accordian 3</span>
-      <p>This is the accordian space!</p>
+      <div>
+        <h3>Subtitle</h3>
+        <p>Paragraph</p>
+      </div>
     </li>
   </ul>
 </section>`)
 );
 
-const shortsVanillaAccordian = () => {
+// Accordian JavaScript
+const shortsVanillaAccordian = (params) => {
+  // Define Params
+  const select = params.selector;
+  const clickable = select + " li *:first-child";
+
+  // Get the notes.. or not.
   let getNodes = (select) => {
     return document.querySelectorAll(select) || [];
   };
 
-  const handleNodeListener = (node, type) => {
-    node.addEventListener(type, (event) => {
-      node.classList.add("classAdded");
-      return node;
+  // Handle Add Event
+  const accordianAdditionManagement = async (event) => {
+    event.target.classList.add("active");
+  };
+
+  // Handle Removal Event
+  const accordianRemovalManagement = async () => {
+    getNodes(clickable).forEach(async (el) => {
+      if (!el.classList.contains("active")) {
+        return;
+      }
+      el.classList.remove("active");
     });
   };
 
-  getNodes(".accordian").forEach((accordian) => {
-    handleNodeListener(accordian, "click");
-  }, this);
+  // * Event Listener Management
+  const handleNodeListener = async (node, type, func) => {
+    node.addEventListener(
+      type,
+      (event) => {
+        func(event);
+      },
+      true
+    );
+  };
+
+  // * Handle Accordian Loops
+  getNodes(clickable).forEach(async (accordian) => {
+    handleNodeListener(accordian, "click", accordianRemovalManagement);
+    handleNodeListener(accordian, "click", accordianAdditionManagement);
+  });
 };
 
 // * Initialisation (Check)
 if (toHtml(false, { render: true })) {
-  shortsVanillaAccordian();
+  // * Init Accordian
+  shortsVanillaAccordian({ selector: ".accordian" });
 }
