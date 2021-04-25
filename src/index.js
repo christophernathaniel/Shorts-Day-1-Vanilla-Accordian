aa83c4da27a42bf59271aa84aa750fc78211a0db
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2021-04-25 20:12:42 +0100

    To Html, Loop Times +  Render Method

--- src/index.js
+++ src/index.js
@@ -1,21 +1,73 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
+//* Our toHtml function is a HTML management function
+const toHtml = (generatedHtml, options = { render: false }) => {
+  // * If array is undefined, define it.
+  window.collectHtml = window.collectHtml || [];
+
+  // * Add to the main HTML collection
+  window.collectHtml.push(generatedHtml);
+
+  // * Return HTML to page
+  const returnHtml = (x) => {
+    document.getElementById("app").innerHTML = x;
+    window.collectHtml = "";
+    return true;
+  };
+
+  // Return Function (true) or false if just collecting
+  return options.render ? returnHtml(window.collectHtml) : false;
+};
+
+// * Our times function creates a loop that'll run x amount of times.
+const times = (x) => (f) => {
+  if (x > 0) {
+    f();
+    times(x - 1)(f);
+  }
+};
+
+// * Render our HTML with 3 times with the toHtml function.
+times(3)(() =>
+  toHtml(`
 <h1>Vanilla Accordian</h1>
-<div>
-  <ul>
+<section>
+  <ul class="accordian">
     <li>
-      <span>Accordian</span>
+      <span>Accordian 1
+      3 4</span>
       <p>This is the accordian space!</p>
     </li>
     <li>
       <span>Accordian 2</span>
-      <p>This is the accordian space(2)!</p>
+      <p>This is the accordian space!</p>
     </li>
     <li>
       <span>Accordian 3</span>
-      <p>This is the accordian space (3)!</p>
+      <p>This is the accordian space!</p>
     </li>
   </ul>
-</div>
-`;
+</section>`)
+);
+
+const shortsVanillaAccordian = () => {
+  let getNodes = (select) => {
+    return document.querySelectorAll(select) || [];
+  };
+
+  const handleNodeListener = (node, type) => {
+    node.addEventListener(type, (event) => {
+      node.classList.add("classAdded");
+      return node;
+    });
+  };
+
+  getNodes(".accordian").forEach((accordian) => {
+    handleNodeListener(accordian, "click");
+  }, this);
+};
+
+// * Initialisation (Check)
+if (toHtml(false, { render: true })) {
+  shortsVanillaAccordian();
+}
