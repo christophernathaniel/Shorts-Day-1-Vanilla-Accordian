f3fdf6f2c5745b9d8f68d7356c6d3584f84e1a21
christophernathaniel <christophernathaniel@outlook.com>
2021-04-27 08:45:50 +0100

    Refactor - Level of depth added

--- src/shortsVanillaAccordian/index.js
+++ src/shortsVanillaAccordian/index.js
@@ -7,28 +7,69 @@
 const shortsVanillaAccordian = (params) => {
   // Define Params
   const select = params.selector;
-  const clickable = select + " li *:first-child";
+  const clickable = select + " li > *:nth-child(1)";
+  const openable = select + " li > *:nth-child(2)";
+  const openableContent = select + " li > *:nth-child(2) > div";
 
   // Get the notes.. or not.
   let getNodes = (select) => {
     return document.querySelectorAll(select) || [];
   };
 
+  // Get Adjacent Node
+  let getAdjacentNode = (elem) => {
+    return elem.nextElementSibling;
+  }
+
+
+  // Takes an array of elements and returns array of heights
+  const retrieveCurrentHeights = (elem) => {
+    const getHeightsList = Array.from(getNodes(elem)).map((item) => {
+     return item.offsetHeight;
+    });
+    return getHeightsList;
+  }
+
+  // Set Default Height
+  const openHeights = retrieveCurrentHeights(openableContent);
+
+  window.resize = function() {
+    openHeights = retrieveCurrentHeights(openableContent)
+  }
+
+  // Find Index
+  function indexInParent(node) {
+    var children = node.parentNode.childNodes;
+    var num = 0;
+    for (var i=0; i<children.length; i++) {
+         if (children[i]==node) return num;
+         if (children[i].nodeType==1) num++;
+    }
+    return -1;
+  }
+
   // Handle Add Event
   const accordianAdditionManagement = async (event) => {
-    event.target.classList.add("active");
+    let tgt = event.target;
+    let node = getAdjacentNode(tgt);
+    tgt.classList.add("active");
+
+    node.style.maxHeight = openHeights[indexInParent(tgt.parentNode)] + 'px';
+    
+    return true;
   };
 
   // Handle Removal Event
-  const accordianRemovalManagement = async () => {
+  const accordianRemovalManagement = async (event) => {
     getNodes(clickable).forEach(async (el) => {
-      if (!el.classList.contains("active")) {
-        return;
+      if (event.target != el) {
+        getAdjacentNode(el).style.maxHeight = "0px";
+        el.classList.remove("active");
       }
-      el.classList.remove("active");
     });
   };
 
+ 
   // * Event Listener Management
   const handleNodeListener = async (node, type, func) => {
     node.addEventListener(
@@ -39,6 +80,12 @@ const shortsVanillaAccordian = (params) => {
       true
     );
   };
+
+  // * Set all heights to 0
+
+  getNodes(openable).forEach((node) => {
+    node.style.maxHeight = "0px";
+  });
 
   // * Handle Accordian Loops
   getNodes(clickable).forEach(async (accordian) => {
